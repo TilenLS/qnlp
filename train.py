1e3fe29b0d8b25c683fb3dec7f2df77ffab609ed
Tilen LS <tilen.stokin@gmail.com>
2024-07-12 12:07:22 +0100

    add utilities file

--- train.py
+++ train.py
@@ -1,31 +1,13 @@
 import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt
-from tqdm import tqdm
 import random
 import datetime
-import os
 import sys
+from lambeq import NumpyModel, AtomicType, Dataset, QuantumTrainer, SPSAOptimizer, IQPAnsatz, BinaryCrossEntropyLoss
+from util import sent2dig, gen_labels, train
 
-from discopro.grammar import tensor
-from discopro.anaphora import connect_anaphora_on_top
-from lambeq import BobcatParser, NumpyModel, AtomicType, Rewriter, Dataset, QuantumTrainer, SPSAOptimizer , AtomicType, IQPAnsatz, RemoveCupsRewriter, UnifyCodomainRewriter, BinaryCrossEntropyLoss
-from lambeq.backend.grammar import Spider, Ty
-from lambeq.backend.quantum import Box, qubit, SelfConjugate, Ry, Diagram
-from contextuality.model import Model, Scenario, CyclicScenario
-
-remove_cups = RemoveCupsRewriter()
-
-parser = BobcatParser()
-rewriter = Rewriter(['auxiliary',
-                     'connector',
-                     'coordination',
-                     'determiner',
-                     'object_rel_pronoun',
-                     'subject_rel_pronoun',
-                     'postadverb',
-                     'preadverb',
-                     'prepositional_phrase'])
+frac = int(sys.argv[1])/100
+join = str(sys.argv[2])
+cut = bool(sys.argv[3])
 
 N = AtomicType.NOUN
 S = AtomicType.SENTENCE
@@ -33,69 +15,14 @@ P = AtomicType.PREPOSITIONAL_PHRASE
 
 ansatz = IQPAnsatz({N: 1, S: 1, P:1}, n_layers=1, n_single_qubit_params=3)
 
-def sent2dig(sentence1: str, sentence2: str, pro: str, ref: str, join: str=False, cut=False):
-    diagram1 = parser.sentence2diagram(sentence1)
-    diagram2 = parser.sentence2diagram(sentence2)
-    diagram = tensor(diagram1, diagram2)
-    
-    if join == 'spider':
-        diagram = diagram >> Spider(S, 2, 1)
-    elif join == 'box':
-        merger = UnifyCodomainRewriter(Ty('s'))
-        diagram = merger(diagram)
-
-    if cut == False:
-        pro_box_idx = next(i for i, box in enumerate(diagram.boxes) if box.name.casefold() == pro.casefold())
-        ref_box_idx = next(i for i, box in enumerate(diagram.boxes) if box.name.casefold() == ref.casefold())
-        diagram = connect_anaphora_on_top(diagram, pro_box_idx, ref_box_idx)
-        
-    diagram = rewriter(remove_cups(diagram)).normal_form()
-    return diagram
-
-def gen_labels(path: str, frac: int, verbose=False, mode=False, cut=False):
-    df = pd.read_csv(path, index_col=0)
-    df = df.sample(frac=frac)
-    
-    if not os.path.exists(os.getcwd()+'/err_logs'):
-        os.mkdir(os.getcwd()+'/err_logs')
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-    f = open("err_logs/log_"+path.split('/')[-1].split('.')[-2]+'_'+timestamp+".txt",'w')
-    
-    circuits, labels, diagrams = [],[],[]
-    for i, row in tqdm(df.iterrows(), total=len(df), position=0, leave=True):
-        col = random.choice(['referent', 'wrong_referent'])
-        sent1, sent2, pro, ref = row[['sentence1', 'sentence2', 'pronoun', col]]
-        
-        label = [[0.25, 0.25],[0.25, 0.25]]
-        if mode == 'spider' or mode == 'box':
-            label = [0, 1] if col == 'referent' else [1,0]
-
-        try:
-            diagram = sent2dig(sent1.strip(), sent2.strip(), pro.strip(), ref.strip(), join=mode, cut=cut)
-            diagrams.append(diagram)
-            circ = ansatz(diagram)
-            circuits.append(circ)
-            labels.append(label)
-        except Exception as err:
-            tqdm.write(f"Error: {err}".strip(), file=f)
-            if verbose:
-                tqdm.write(f"Error: {err}".strip(), file=sys.stderr)
-    f.close()
-    
-    return circuits, labels, diagrams
-
 print("Generating diagrams and converting to circuits:")
-frac = int(sys.argv[1])/100
-mode = str(sys.argv[2])
-cut = bool(sys.argv[3])
-#mode = input("Choose diagram type (default, spider, box): ")
-#frac = int(input("What % of data to use? (<100) "))/100
+
 train_circuits, train_labels, train_diagrams = gen_labels('dataset/original_data/train.csv', 
-                                                          frac=frac, mode=mode, cut=cut)
+                                                          frac=frac, join=join, cut=cut)
 val_circuits, val_labels, val_diagrams = gen_labels('dataset/original_data/val.csv', 
-                                                    frac=frac, mode=mode, cut=cut)
+                                                    frac=frac, join=join, cut=cut)
 test_circuits, test_labels, test_diagrams = gen_labels('dataset/original_data/test.csv', 
-                                                       frac=frac, mode=mode, cut=cut)
+                                                       frac=frac, join=join, cut=cut)
 
 model = NumpyModel.from_diagrams(train_circuits + val_circuits + test_circuits, use_jit=False)
 loss = BinaryCrossEntropyLoss(use_jax=True)
@@ -119,25 +46,8 @@ trainer = QuantumTrainer(model,
                          verbose='text', 
                          seed=SEED)
 
-def train(trainer: QuantumTrainer, EPOCH_ARR: [int], BATCH_ARR: [int], SEED_N: int, train_dataset: Dataset, val_dataset: Dataset, test_dataset: Dataset):
-    SEEDS = random.sample(range(1000), SEED_N)
-    trainer.verbose = 'supress'
-    model = trainer.model
-    
-    print("%0s %23s %7s %7s  %12s" % ("Time","Epochs","Batch","Seed","Accuracy"))
-    for EPOCHS in EPOCH_ARR:
-        for BATCH_SIZE in BATCH_ARR:
-            for SEED in SEEDS:
-                trainer.epochs = EPOCHS
-                trainer.optim_hyperparams = {'a': 0.1, 'c': 0.06, 'A': 0.01 * EPOCHS}
-                train_dataset.batch_size = BATCH_SIZE
-                time = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-                print("%0s %8s %7s %7s" % (time, EPOCHS, BATCH_SIZE, SEED), end='')
-                trainer.fit(train_dataset, val_dataset, eval_interval=1, log_interval=1)
-                test_acc = acc(model(test_dataset.data), test_dataset.targets)
-                print("%14s" % (round(test_acc, 6)))
-
 print("Learning parameters: "+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S"))
 trainer.fit(train_dataset, val_dataset, eval_interval=1, log_interval=1)
 test_acc = acc(model(test_dataset.data), test_dataset.targets)
 print('Test accuracy:', test_acc)
+#train(trainer, [50,100,150,200], [5,10,20,30], random.sample(range(500), 5), train_dataset, val_dataset, test_dataset)
---
+++ util.py
@@ -0,0 +1,95 @@
+from discopro.grammar import tensor
+from discopro.anaphora import connect_anaphora_on_top
+from lambeq import BobcatParser, AtomicType, RemoveCupsRewriter, UnifyCodomainRewriter, Rewriter, QuantumTrainer, Dataset
+from lambeq.backend.grammar import Spider
+import pandas as pd 
+import random
+from tqdm import tqdm
+import os
+import sys
+
+remove_cups = RemoveCupsRewriter()
+
+parser = BobcatParser()
+rewriter = Rewriter(['auxiliary',
+                     'connector',
+                     'coordination',
+                     'determiner',
+                     'object_rel_pronoun',
+                     'subject_rel_pronoun',
+                     'postadverb',
+                     'preadverb',
+                     'prepositional_phrase'])
+
+N = AtomicType.NOUN
+S = AtomicType.SENTENCE
+P = AtomicType.PREPOSITIONAL_PHRASE
+
+def sent2dig(sentence1: str, sentence2: str, pro: str, ref: str, join=None, cut=False):
+    diagram1 = parser.sentence2diagram(sentence1)
+    diagram2 = parser.sentence2diagram(sentence2)
+    diagram = tensor(diagram1, diagram2)
+    
+    if join == 'spider':
+        diagram = diagram >> Spider(S, 2, 1)
+    elif join == 'box':
+        merger = UnifyCodomainRewriter(S)
+        diagram = merger(diagram)
+
+    if not cut:
+        pro_box_idx = next(i for i, box in enumerate(diagram.boxes) if box.name.casefold() == pro.casefold())
+        ref_box_idx = next(i for i, box in enumerate(diagram.boxes) if box.name.casefold() == ref.casefold())
+        diagram = connect_anaphora(diagram, pro_box_idx, ref_box_idx)
+        
+    diagram = rewriter(remove_cups(diagram)).normal_form()
+    return diagram
+
+def gen_labels(path: str, verbose=False, frac=1, join=None, cut=False):
+    df = pd.read_csv(path, index_col=0)
+    df = df.sample(frac=frac)
+    
+    if not os.path.exists(os.getcwd()+'/err_logs'):
+        os.mkdir(os.getcwd()+'/err_logs')
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+    f = open("err_logs/log_"+path.split('/')[-1].split('.')[-2]+'_'+timestamp+".txt",'w')
+    
+    circuits, labels, diagrams = [],[],[]
+    for i, row in tqdm(df.iterrows(), total=len(df), position=0, leave=True):
+        col = random.choice(['referent', 'wrong_referent'])
+        sent1, sent2, pro, ref = row[['sentence1', 'sentence2', 'pronoun', col]]
+        
+        label = [[0.25, 0.25],[0.25, 0.25]]
+        if mode == 'spider' or mode == 'box':
+            label = [0, 1] if col == 'referent' else [1,0]
+
+        try:
+            diagram = sent2dig(sent1.strip(), sent2.strip(), pro.strip(), ref.strip(), join=join, cut=cut)
+            diagrams.append(diagram)
+            circ = ansatz(diagram)
+            circuits.append(circ)
+            labels.append(label)
+        except Exception as err:
+            tqdm.write(f"Error: {err}".strip(), file=f)
+            if verbose:
+                tqdm.write(f"Error: {err}".strip(), file=sys.stderr)
+    f.close()
+    
+    return circuits, labels, diagrams
+
+def train(trainer: QuantumTrainer, EPOCH_ARR: [int], BATCH_ARR: [int], SEED_N: int, train_dataset: Dataset, val_dataset: Dataset, test_dataset: Dataset):
+    SEEDS = random.sample(range(1000), SEED_N)
+    trainer.verbose = 'supress'
+    model = trainer.model
+    
+    print("%0s %23s %7s %7s  %12s" % ("Time","Epochs","Batch","Seed","Accuracy"))
+    for EPOCHS in EPOCH_ARR:
+        for BATCH_SIZE in BATCH_ARR:
+            for SEED in SEEDS:
+                trainer.epochs = EPOCHS
+                trainer.optim_hyperparams = {'a': 0.1, 'c': 0.06, 'A': 0.01 * EPOCHS}
+                train_dataset.batch_size = BATCH_SIZE
+                time = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
+                print("%0s %8s %7s %7s" % (time, EPOCHS, BATCH_SIZE, SEED), end='')
+                trainer.fit(train_dataset, val_dataset, eval_interval=1, log_interval=1)
+                test_acc = acc(model(test_dataset.data), test_dataset.targets)
+                print("%14s" % (round(test_acc, 6)))
