8973ddd93e25afcb1f9986f349ade08550e308b5
Tilen LS <tilen.stokin@gmail.com>
2024-10-30 12:11:47 +0000

    update code

--- gen_data.py
+++ gen_data.py
@@ -14,25 +14,30 @@ P = AtomicType.PREPOSITIONAL_PHRASE
 
 ansatz = Sim14Ansatz({N: 1, S: 1, P:1}, n_layers=1, n_single_qubit_params=3)
 
-path = ""
+path = "data/og_data"
 
-df = pd.read_csv(path, index_col=0)
-df = df.sample(frac=frac)
+gen_data(dir_path, file_name):
+    df = pd.read_csv(path + '/' + file_name + '.csv', index_col=0)
+    df = df.sample(frac=frac)
+    
+    circuits, labels, diagrams = [],[],[]
+    
+    for i, row in tqdm(df.iterrows(), total=len(df), position=0, leave=True):
+        col = random.choice(['referent', 'wrong_referent'])
+        sent1, sent2, pro, ref = row[['sentence1', 'sentence2', 'pronoun', col]]
+        label = [0, 1] if col == 'referent' else [1,0]
+        try:
+            diagram = sent2dig(sent1.strip(), sent2.strip(), pro.strip(), ref.strip(), sent_model='spider', con_ref=True)
+            diagrams.append(diagram)
+            circuits.append(ansatz(diagram))
+            labels.append(label)
+        except Exception as err:
+            tqdm.write(f"Error: {err}".strip(), file=sys.stderr)
+    
+    f = open('dataset/data_'+file_name+'-'+datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")+'.pkl', 'wb')
+    pickle.dump(list(zip(circuits, labels, diagrams)), f)
+    f.close()
 
-circuits, labels, diagrams = [],[],[]
-
-for i, row in tqdm(df.iterrows(), total=len(df), position=0, leave=True):
-    col = random.choice(['referent', 'wrong_referent'])
-    sent1, sent2, pro, ref = row[['sentence1', 'sentence2', 'pronoun', col]]
-    label = [0, 1] if col == 'referent' else [1,0]
-    try:
-        diagram = sent2dig(sent1.strip(), sent2.strip(), pro.strip(), ref.strip(), sent_model='spider', con_ref=True)
-        diagrams.append(diagram)
-        circuits.append(ansatz(diagram))
-        labels.append(label)
-    except Exception as err:
-        tqdm.write(f"Error: {err}".strip(), file=sys.stderr)
-
-f = open('dataset/data_cld_'+datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")+'.pkl', 'wb')
-pickle.dump(list(zip(circuits, labels, diagrams)), f)
-f.close()
+gen_data(path, 'train')
+gen_data(path, 'val')
+gen_data(path, 'test')
